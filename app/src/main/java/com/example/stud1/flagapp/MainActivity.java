70be4707ed759176d9cf5f8e1402fa3fef5bf726
Elena <koroteevaelena@mail.ru>
2019-03-07 21:58:51 +0300

    add logic

--- app/src/main/java/com/example/stud1/flagapp/MainActivity.java
+++ app/src/main/java/com/example/stud1/flagapp/MainActivity.java
@@ -1,22 +1,63 @@
 package com.example.stud1.flagapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button button_show;
+    EditText searchCountry;
+    ImageView flagImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button button = (Button)findViewById(R.id.button);
-        final EditText editText = (EditText) findViewById(R.id.editText);
-        String countryName = editText.getText().toString();
+       button_show = findViewById(R.id.button);
+       searchCountry = findViewById(R.id.searchCountry);
 
+       searchCountry.addTextChangedListener(mWatcher);
+
+       flagImage = findViewById(R.id.flagView);
+       button_show.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               if (searchCountry.getText().toString().equals("Россия")){
+                   flagImage.setImageResource(R.drawable.flag_rf);
+               }else{
+                   Toast.makeText(MainActivity.this,"This country does not exist:(",Toast.LENGTH_SHORT).show();;
+               }
+
+           }
+       });
 
     }
+TextWatcher mWatcher = new TextWatcher() {
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+ if (!searchCountry.getText().toString().isEmpty()){
+     button_show.setEnabled(true);
+     button_show.setBackgroundResource(R.drawable.btn_gradient);
+
+ }
+    }
+};
 }
